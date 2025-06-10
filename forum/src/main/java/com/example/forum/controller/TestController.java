82b1f0ab443f60fbc25325c6ef35eb654293f9e8
bo wen <18272262790@163.com>
2025-06-10 20:12:04 +0800

    Changes

--- forum/src/main/java/com/example/forum/controller/TestController.java
+++ forum/src/main/java/com/example/forum/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.forum.demos.web.controller;
+package com.example.forum.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- forum/src/test/java/com/example/forum/ForumApplicationTests.java
+++ forum/src/test/java/com/example/forum/ForumApplicationTests.java
@@ -1,11 +1,41 @@
 package com.example.forum;
 
+import com.example.forum.dao.UserMapper;
+import com.example.forum.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootTest
 class ForumApplicationTests {
+    // 数据源
+    @Resource
+    private DataSource dataSource;
 
+    // 用户的Mapper
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    void testConnection() throws SQLException {
+        System.out.println("dataSource = "+ dataSource.getClass());
+        // 获取数据库连接
+        Connection connection = dataSource.getConnection();
+        System.out.println("connection = "+ connection);
+        connection.close();
+
+    }
+
+    @Test
+    void testMybatis() {
+        User user = userMapper.selectByPrimaryKey(1L);
+        System.out.println(user);
+        System.out.println(user.getUsername());
+    }
     @Test
     void contextLoads() {
         System.out.println("TEST: spring");
