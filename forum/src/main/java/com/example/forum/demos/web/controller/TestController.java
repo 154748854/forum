ba53d30efcf4b68865dfcfe0bd8ea7d10b26e45d
bo wen <18272262790@163.com>
2025-06-10 16:22:35 +0800

    第一次提交

---
+++ forum/src/main/java/com/example/forum/demos/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.forum.demos.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+// 表示返回的结果是数据
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("test1")
+    public String test1() {
+        return "hello, Spring";
+    }
+}
